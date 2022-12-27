a8c37ffb5e8a646b6ea547d0ae30e6bdbc2cc651
Bedirhan Köse <bedirhan.kose05@gmail.com>
2022-12-27 23:02:38 +0100

    Updated
    Gesture recognizer and navigation controller have been added.

--- ShoppingListCD/DetailsViewController.swift
+++ ShoppingListCD/DetailsViewController.swift
@@ -11,17 +11,20 @@ class DetailsViewController: UIViewController {
 
     
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var nameProduct: UITextField!
-    
     @IBOutlet weak var priceProduct: UITextField!
-    
     @IBOutlet weak var sizeProduct: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeTheKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func closeTheKeyboard() {
+        view.endEditing(true)
+        
     }
     
     @IBAction func SaveButton(_ sender: Any) {
--- ShoppingListCD/ViewController.swift
+++ ShoppingListCD/ViewController.swift
@@ -11,11 +11,19 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonTapped))
+        
     }
-
-
+    @objc func addButtonTapped() {
+        
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+        
+    }
+    
 }
 
