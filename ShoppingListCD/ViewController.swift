680d1abee04c10a2d763b0abe3d6f2d3ec6008a4
Bedirhan Köse <bedirhan.kose05@gmail.com>
2022-12-28 23:03:34 +0100

    Data has been successfully retrieved.

--- ShoppingListCD/ViewController.swift
+++ ShoppingListCD/ViewController.swift
@@ -6,23 +6,72 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var nameArray = [String]()
+    var idArray = [UUID]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonTapped))
         
+        
+        getDatas()
     }
+    
+    
+    func getDatas() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ShoppingDatabase")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            for result in results as! [NSManagedObject] {
+                if let name = result.value(forKey: "name") as? String {
+                    nameArray.append(name)
+                }
+                
+                if let id = result.value(forKey: "id") as? UUID {
+                    idArray.append(id)
+                }
+                    
+            }
+            
+            tableView.reloadData()
+            
+        } catch {
+            print("Error")
+        }
+        
+        
+    }
+    
     @objc func addButtonTapped() {
         
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
         
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
     }
     
 }
