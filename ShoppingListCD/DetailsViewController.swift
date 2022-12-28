41ede3ce07b3714cf8d361966be9a575b9367b92
Bedirhan Köse <bedirhan.kose05@gmail.com>
2022-12-28 23:46:57 +0100

    Datas has been filtered.

--- ShoppingListCD/DetailsViewController.swift
+++ ShoppingListCD/DetailsViewController.swift
@@ -25,7 +25,42 @@ class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, 
         if ChosenProductName != "" {
             //It shows CoreData chosen product information.
             if let uuidString = ChosenProductID?.uuidString {
-                print(uuidString)
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                let context = appDelegate.persistentContainer.viewContext
+                
+                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ShoppingDatabase")
+                fetchRequest.predicate = NSPredicate(format: "id = %@", uuidString)
+                fetchRequest.returnsObjectsAsFaults = false
+                
+                do {
+                    let results = try context.fetch(fetchRequest)
+                    
+                    if results.count > 0 {
+                        for result in results as! [NSManagedObject] {
+                            if let name = result.value(forKey: "name") as? String {
+                                nameProduct.text = name
+                            }
+                            
+                            if let price = result.value(forKey: "price") as? Int {
+                                priceProduct.text = String(price)
+                            }
+                            
+                            if let size = result.value(forKey: "size") as? String {
+                                sizeProduct.text = size
+                            }
+                            
+                            if let imageData = result.value(forKey: "image") as? Data {
+                                let image = UIImage(data: imageData)
+                                imageView.image = image
+                            }
+                        }
+                    }
+                
+                }catch {
+                    print("error")
+                }
+                
+                
             }
             
         } else{
